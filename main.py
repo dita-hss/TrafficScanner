953a56f368d999a4efe01a9924000e7430dadc1b
amanda hernandez sandate <amandahernandezsandate@amandas-air.attlocal.net>
2023-05-22 21:08:15 -0500

    masking

--- main.py
+++ main.py
@@ -39,18 +39,24 @@ while True:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1, y1, w, h), l=8)
-
+        
             #confidennce
             conf = math.ceil((box.conf[0] * 100)) / 100
 
             #class name
             cls = int(box.cls[0])
-            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), 
+            currentClass = classNames[cls]
+
+            if (currentClass == "car" or 
+                currentClass == "truck" or 
+                currentClass == "bus" or 
+                currentClass == "motorbike") and (conf > 0.3):
+                cvzone.cornerRect(img, (x1, y1, w, h), l=8)
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), 
                                scale=0.8, thickness=1, offset=3)
 
     fps = 1 / (new_frame_time - prev_frame_time)
     prev_frame_time = new_frame_time
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
